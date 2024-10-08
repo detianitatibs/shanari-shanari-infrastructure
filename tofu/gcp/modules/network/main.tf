9a9846cd3a9c9076654293a999262eba7fcecf3e
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-10-08 23:08:21 +0900

    エラー回避のためにレンジから外す

--- tofu/gcp/modules/network/main.tf
+++ tofu/gcp/modules/network/main.tf
@@ -31,5 +31,6 @@ resource "google_compute_global_address" "private_ip_address" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  # reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  reserved_peering_ranges = []
 }
