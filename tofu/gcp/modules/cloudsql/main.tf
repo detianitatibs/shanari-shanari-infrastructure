bf15737e15bdbfda94ccc67590aba6e85b261d6a
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-10-08 22:14:26 +0900

    CloudSQLを追加

---
+++ tofu/gcp/modules/cloudsql/main.tf
@@ -0,0 +1,36 @@
+# CloudSQL for Postgres
+resource "google_sql_database" "postgres" {
+  name     = "${var.app_name}-cloudsql-postgres"
+  instance = google_sql_database_instance.postgres.name
+}
+
+resource "google_sql_database_instance" "postgres" {
+  name                 = "${var.app_name}-cloudsql-postgres-instance"
+  region               = "${var.gcp_region}"
+  database_version     = "POSTGRES_16"
+  deletion_protection  = "false"
+
+  settings {
+    tier              = "db-f1-micro"
+    disk_size         = 10
+    disk_autoresize   = true
+    availability_type = "REGIONAL"
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = "${var.subnet_private_1_self_link}"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      name,
+      disk_size
+    ]
+  }
+}
+
+resource "google_sql_user" "admin" {
+  name     = "${var.admin_user}"
+  instance = google_sql_database_instance.postgres.name
+  password = "${var.admin_password}"
+}
--- tofu/gcp/modules/cloudsql/variables.tf
+++ tofu/gcp/modules/cloudsql/variables.tf
@@ -1,28 +1,34 @@
-variable "APP_NAME" {
+variable "app_name" {
   type        = string
   description = "The name of this application"
   sensitive   = true
 }
 
-variable "GCP_PROJECT_NAME" {
-  type        = string
-  description = "The name of GCP Project"
-  sensitive   = true
-}
-
-variable "GCP_REAGION" {
+variable "gcp_region" {
   type        = string
   description = "The value of GCP Region"
   sensitive   = true
 }
 
-variable "POSTGRES_USER" {
+variable "vpc_self_link" {
+  type        = string
+  description = "The self_link of vpc"
+  sensitive   = true
+}
+
+variable "subnet_private_1_self_link" {
+  type        = string
+  description = "The self_link of private subnet1"
+  sensitive   = true
+}
+
+variable "admin_user" {
   type        = string
   description = "The value of CloudSQL for Postgres's admin user"
   sensitive   = true
 }
 
-variable "POSTGRES_PASSWORD" {
+variable "admin_password" {
   type        = string
   description = "The value of CloudSQL for Postgres's admin password"
   sensitive   = true
