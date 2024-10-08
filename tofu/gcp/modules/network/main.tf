7f34a63dae0b0eb8407bcca2a950070c71ada5d6
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2024-10-08 23:08:58 +0900

    reserved_peering_rangesをもとに戻し

--- tofu/gcp/modules/network/main.tf
+++ tofu/gcp/modules/network/main.tf
@@ -31,6 +31,5 @@ resource "google_compute_global_address" "private_ip_address" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc.id
   service                 = "servicenetworking.googleapis.com"
-  # reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-  reserved_peering_ranges = []
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
